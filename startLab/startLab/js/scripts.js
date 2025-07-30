4f2cdae12d7139f7598244ab9f550946079c40d0
alicemartins-hue <amartins1@byupathway.edu>
2025-07-30 13:52:47 +0100

    modal pt 2

--- startLab/startLab/js/scripts.js
+++ startLab/startLab/js/scripts.js
@@ -3,8 +3,18 @@ const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
 const dialogText = document.querySelector("#dialogBox div");
 
-openButton.addEventListener("click", () => {
+
+openButton1.addEventListener("click", () => {
     dialogBox.showModal();
+    dialogText.innerHTML = 'One aple contains 95 calories'
+});
+openButton2.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = 'Stevee le poisson steveee et orange'
+});
+openButton3.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = 'I love you Calrson, all this time when I was asking for bananas I had think that u would understand that this was just a code to say I want u I need u I love u, our three a, our bananas.'
 });
 
 closeButton.addEventListener("click", () => {

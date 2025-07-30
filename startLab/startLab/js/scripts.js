cbca40db72cd19629c3b48b6f0716068f1c61a97
alicemartins-hue <amartins1@byupathway.edu>
2025-07-30 11:46:27 +0100

    js

--- startLab/startLab/js/scripts.js
+++ startLab/startLab/js/scripts.js
@@ -1,6 +1,7 @@
 const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
+const dialogText = document.querySelector("#dialogBox div");
 
 openButton.addEventListener("click", () => {
     dialogBox.showModal();

513845555472b7078ffbbd1121f879bc6b2c1202
alicemartins-hue <amartins1@byupathway.edu>
2025-08-07 12:10:47 +0100

    Update discover.html title and improve layout structure in places.js

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -1,15 +1,31 @@
 import { places } from '../data/places.mjs'
 console.log(places)
 
-const showHere = document.querySelector("#allplaces");
+const showHere = document.querySelector("#discover")
 
 function displayItems(places) {
     places.forEach(place => {
-        const thecard = document.createElement('div');
-        const thephoto = document.createElement('img');
-        const thetitle = document.createElement('h2');
-        const address = document.createElement('p');
-        const thedesc = document.createElement('p');
-        
-    });
+        const thecard = document.createElement('div')
+
+        const thephoto = document.createElement('img')
+        thephoto.src = `images/${place.photo}`
+        thephoto.alt = place.name
+        thecard.appendChild(thephoto)
+
+        const thetitle = document.createElement('h2')
+        thetitle.innerText = place.name
+        thecard.appendChild(thetitle)
+
+        const theaddress = document.createElement('p')
+        theaddress.innerText = place.address
+        thecard.appendChild(theaddress)
+
+        const thedesc = document.createElement('p')
+        thedesc.innerText = place.description
+        thecard.appendChild(thedesc)
+
+        showHere.appendChild(thecard)   
+    })
 }
+
+displayItems(places)

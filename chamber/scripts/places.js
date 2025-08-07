df72501d4de63813e20d80f401f1488303e96037
alicemartins-hue <amartins1@byupathway.edu>
2025-08-07 20:25:24 +0100

    Add lazy loading attribute to images in displayItems function for improved performance

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -10,6 +10,7 @@ function displayItems(places) {
         const thephoto = document.createElement('img')
         thephoto.src = `images/${place.photo}`
         thephoto.alt = place.name
+        thephoto.loading = 'lazy';
         thecard.appendChild(thephoto)
 
         const thetitle = document.createElement('h2')

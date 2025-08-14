15286825e799f406b257a593449a2338c1d1d69a
alicemartins-hue <amartins1@byupathway.edu>
2025-08-14 16:42:30 +0100

    Refactor book display functionality and add dialog for book details; update styles for improved user experience

--- FinalProject/scripts/bookweek.js
+++ FinalProject/scripts/bookweek.js
@@ -1,3 +1,4 @@
+import { dialogBooksDescription } from './dialog.js';
 const list = 'data/books.json';
 async function getBooks() {
     try {
@@ -33,28 +34,5 @@ function renderSpotlight(books) {
     container.appendChild(card);
 
 }
-
-
-const modal = document.getElementById('book-details');
-function dialogBooksDescription(book) {
-    modal.innerHTML = '';
-    modal.innerHTML = `
-    <button id="closeModal" aria-label="Close">❌</button>
-    <h2><a href="${book.purchase_link}" target="_blank"> ${book.title}</a></h2>
-    <h3>Author ${book.author}</h3>
-    <p><strong>Description</strong></p>
-    <p>${book.description}</p>
-    <h4>The book was inspired by ${book.war_conflict}</h4>
-    <h4>The war took place during the period: ${book.war_period} in ${book.country}</h3>   
-    <a href="${book.purchase_link}" target="_blank"><button>Purchase Here</button></a>
-    `;
-    modal.showModal();
-
-    const closeBtn = modal.querySelector('#closeModal');
-    closeModal.addEventListener("click", () => {
-        modal.close();
-    });
-}
-
 
 getBooks();

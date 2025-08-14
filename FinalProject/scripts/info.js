dbdb052a25bd55d2118225cfc836a4a2286ee88e
alicemartins-hue <amartins1@byupathway.edu>
2025-08-14 17:25:06 +0100

    fixing the mistake i made trying to fix it

--- FinalProject/scripts/info.js
+++ FinalProject/scripts/info.js
@@ -1,4 +1,6 @@
 const myInfo = new URLSearchParams(window.location.search);
+console.log(myInfo)
+
 document.querySelector('#results').innerHTML = `
 <p>This is a <strong>confirmation</strong> that the person <strong>${myInfo.get('firstName')} ${myInfo.get('lastName')} </strong> had submitted <strong>${myInfo.get('bookTitle')}</strong> by <strong>${myInfo.get('author')}</strong> as a book recommendation.</p>
 <p>Lets confirm your individual information:</p>
